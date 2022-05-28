59f9d8c8c9d76cbe2756cf63334f6911f6aebd7c
adriaqf <adriaqf@gmail.com>
2022-05-28 03:40:19 +0200

    conflict solved notificar

--- src/main/java/es/http/service/dto/Notificar.java
+++ src/main/java/es/http/service/dto/Notificar.java
@@ -25,45 +25,24 @@ public class Notificar {
 	private int estado;
 	@Column(name="fecha")
 	private Date fecha;
-<<<<<<< HEAD
-=======
-	@Column(name ="estado")
-	private int estado;
 	@Column(name ="titulo_libro")
 	private String titulo_libro;
-	
-	//TODO revisar @ManyToMany
-/*	@ManyToMany
-	@JoinColumn(name="usuario_emisor_id")
-	Usuario usuario_emisor_id;
->>>>>>> branch '1Entitad-Usuario' of https://github.com/sigala1990/Proyecto_API_BootCamp22.git
-	
+		
 	@OneToMany
 	@JoinColumn(name="usuario_emisor_id")
 	private List<Usuario> usuario_emisor_id;
 	
 	@OneToMany
 	@JoinColumn(name="usuario_receptor_id")
-<<<<<<< HEAD
 	private List<Usuario>usuario_receptor_id;
 
-=======
-	Usuario usuario_receptor_id;
-*/
->>>>>>> branch '1Entitad-Usuario' of https://github.com/sigala1990/Proyecto_API_BootCamp22.git
 	public Notificar() {
 		super();
 	}
 
-<<<<<<< HEAD
+
 	public Notificar(int id, String mensaje, int estado, Date fecha, List<Usuario> usuario_emisor_id,
 			List<Usuario> usuario_receptor_id) {
-=======
-
-
-	public Notificar(int id, String mensaje, Date fecha, int estado, String titulo_libro/*, Usuario usuario_emisor_id,
-			Usuario usuario_receptor_id*/) {
->>>>>>> branch '1Entitad-Usuario' of https://github.com/sigala1990/Proyecto_API_BootCamp22.git
 		super();
 		this.id = id;
 		this.mensaje = mensaje;
@@ -75,15 +54,6 @@ public class Notificar {
 		this.usuario_receptor_id = usuario_receptor_id;*/
 	}
 
-
-
-	public int getEstado() {
-		return estado;
-	}
-
-	public void setEstado(int estado) {
-		this.estado = estado;
-	}
 
 	public String getTitulo_libro() {
 		return titulo_libro;
@@ -124,13 +94,9 @@ public class Notificar {
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
-<<<<<<< HEAD
 
 	public List<Usuario> getUsuario_emisor_id() {
-=======
-/*
-	public Usuario getUsuario_emisor_id() {
->>>>>>> branch '1Entitad-Usuario' of https://github.com/sigala1990/Proyecto_API_BootCamp22.git
+
 		return usuario_emisor_id;
 	}
 
@@ -145,10 +111,5 @@ public class Notificar {
 	public void setUsuario_receptor_id(List<Usuario> usuario_receptor_id) {
 		this.usuario_receptor_id = usuario_receptor_id;
 	}
-<<<<<<< HEAD
 
-=======
-	*/
-	
->>>>>>> branch '1Entitad-Usuario' of https://github.com/sigala1990/Proyecto_API_BootCamp22.git
 }
