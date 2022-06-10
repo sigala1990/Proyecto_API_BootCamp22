7165d646b44d70922f3e4899efee54da40c172a5
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-06-10 13:36:18 +0200

    Cambiada la entidad libro en la rama editorial

--- src/main/java/es/http/service/controller/LibroController.java
+++ src/main/java/es/http/service/controller/LibroController.java
@@ -1,8 +1,11 @@
 package es.http.service.controller;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,58 +28,90 @@ public class LibroController {
 	
 	@GetMapping("/libro")
 	@PreAuthorize("hasAnyAuthority('ADMIN')")
-	public List<Libro> listarLibro(){
-		return libroServiceImpl.listarLibro();
+	public ResponseEntity<List<Libro>> listarLibro(){
+		return new ResponseEntity<>(libroServiceImpl.listarLibro(), HttpStatus.OK);
 	}
 	
 	
 	@PostMapping("/libro")
-	public Libro salvarLibro(@RequestBody Libro Libro) {
+	public ResponseEntity<Libro> salvarLibro(@RequestBody Libro Libro) {
+				
+		return new ResponseEntity<>(libroServiceImpl.guardarLibro(Libro), HttpStatus.CREATED);
 		
-		return libroServiceImpl.guardarLibro(Libro);
 	}
 	
 	
 	@GetMapping("/libro/{id}")
-	public Libro LibroXID(@PathVariable(name="id") int id) {
+	public ResponseEntity<Libro>  LibroXID(@PathVariable(name="id") int id) {
 		
-		Libro Libro_xid= new Libro();
-		
-		Libro_xid=libroServiceImpl.LibroXID(id);
+		try {
+			
+			Libro libro_xid= new Libro();
+			
+			libro_xid = libroServiceImpl.LibroXID(id);
+			
+			return new ResponseEntity<>(libro_xid, HttpStatus.OK);
+			
+		} catch (NoSuchElementException e) {
+			
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			
+		}
 				
-		return Libro_xid;
+		
 	}
 	
 	@PutMapping("/libro/{id}")
-	public Libro actualizarLibro(@PathVariable(name="id")int id,@RequestBody Libro Libro) {
+	public ResponseEntity<Libro> actualizarLibro(@PathVariable(name="id")int id,@RequestBody Libro Libro) {
 		
-		Libro Libro_seleccionado= new Libro();
-		Libro Libro_actualizado= new Libro();
-		
-		Libro_seleccionado= libroServiceImpl.LibroXID(id);
-		
-		Libro_seleccionado.setAutor(Libro.getAutor());
-		Libro_seleccionado.setTitulo(Libro.getTitulo());
-		Libro_seleccionado.setIsbn(Libro.getIsbn());
-		Libro_seleccionado.setEdad(Libro.getEdad());
-		Libro_seleccionado.setCategoria(Libro.getCategoria());
-		Libro_seleccionado.setDuracion_reserva_dias(Libro.getDuracion_reserva_dias());
-		Libro_seleccionado.setCantidad_veces_reservado(Libro.getCantidad_veces_reservado());
-		Libro_seleccionado.setUrl_img(Libro.getUrl_img());
-		Libro_seleccionado.setUsuario(Libro.getUsuario());
-		Libro_seleccionado.setEditorial(Libro.getEditorial());
-		Libro_seleccionado.setDescripcion(Libro.getDescripcion());
-		Libro_seleccionado.setDisponible(Libro.getDisponible());
-		
-		
-		Libro_actualizado = libroServiceImpl.actualizarLibro(Libro_seleccionado);
-				
-		return Libro_actualizado;
+		try {
+			
+			Libro Libro_seleccionado = new Libro();
+			Libro Libro_actualizado = new Libro();
+			
+			Libro_seleccionado = libroServiceImpl.LibroXID(id);
+			
+			Libro_seleccionado.setAutor(Libro.getAutor());
+			Libro_seleccionado.setTitulo(Libro.getTitulo());
+			Libro_seleccionado.setIsbn(Libro.getIsbn());
+			Libro_seleccionado.setEdad(Libro.getEdad());
+			Libro_seleccionado.setCategoria(Libro.getCategoria());
+			Libro_seleccionado.setDuracion_reserva_dias(Libro.getDuracion_reserva_dias());
+			Libro_seleccionado.setCantidad_veces_reservado(Libro.getCantidad_veces_reservado());
+			Libro_seleccionado.setUrl_img(Libro.getUrl_img());
+			Libro_seleccionado.setUsuario(Libro.getUsuario());
+			Libro_seleccionado.setEditorial(Libro.getEditorial());
+			Libro_seleccionado.setDescripcion(Libro.getDescripcion());
+			Libro_seleccionado.setDisponible(Libro.getDisponible());
+			
+			
+			Libro_actualizado = libroServiceImpl.actualizarLibro(Libro_seleccionado);
+			
+			return new ResponseEntity<>(Libro_actualizado, HttpStatus.OK);
+			
+		} catch (NoSuchElementException e) {
+			
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			
+		}
+
 	}
 	
 	@DeleteMapping("/libro/{id}")
-	public void eleiminarLibro(@PathVariable(name="id")int id) {
-		libroServiceImpl.eliminarLibro(id);
+	public ResponseEntity<String> eleiminarLibro(@PathVariable(name="id")int id) {
+		
+		try {
+			
+			libroServiceImpl.LibroXID(id);
+
+			libroServiceImpl.eliminarLibro(id);
+			return new ResponseEntity<>("El libro se ha eliminado correctamente", HttpStatus.OK);
+			
+		} catch (NoSuchElementException e) {
+			
+			return new ResponseEntity<>("No se ha encontrado el libro",HttpStatus.NOT_FOUND);
+			
+		}
 	}
 
 }
