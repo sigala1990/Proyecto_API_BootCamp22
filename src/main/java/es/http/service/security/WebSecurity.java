009b16c1dcb7b220b684f3e690060d2019a71605
adriaqf <adriaqf@gmail.com>
2022-06-25 01:42:07 +0200

    CORS

--- src/main/java/es/http/service/security/WebSecurity.java
+++ src/main/java/es/http/service/security/WebSecurity.java
@@ -1,3 +1,4 @@
+<<<<<<< Upstream, based on branch 'development' of https://github.com/sigala1990/Proyecto_API_BootCamp22.git
 package es.http.service.security;
 
 import static es.http.service.security.Constants.LOGIN_URL;
@@ -95,4 +96,92 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
 		return source;
 	}
+=======
+package es.http.service.security;
+
+import static es.http.service.security.Constants.LOGIN_URL;
+import static es.http.service.security.Constants.SWAGGER_RESOURCES;
+import static es.http.service.security.Constants.SWAGGER_UI_HTML;
+import static es.http.service.security.Constants.SWAGGER_API_DOCS;
+import static es.http.service.security.Constants.SWAGGER_UI;
+import static es.http.service.security.Constants.SWAGGER_WEBJARS;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.servlet.Filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+	private UserDetailsService userDetailsService;
+	private Filter simpleCorsFilter;
+
+	public WebSecurity(UserDetailsService userDetailsService) {
+		this.userDetailsService = userDetailsService;
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Autowired
+	public SimpleCORSFilter myCorsFilter;
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		/*
+		 * 1. Se desactiva el uso de cookies
+		 * 2. Se activa la configuración CORS con los valores por defecto
+		 * 3. Se desactiva el filtro CSRF
+		 * 4. Se indica que el login no requiere autenticación
+		 * 5. Se indica que el resto de URLs esten securizadas
+		 */
+		httpSecurity
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.cors().and()
+			.csrf().disable()
+			.authorizeRequests()
+			.antMatchers(HttpMethod.POST, LOGIN_URL).permitAll()
+			.anyRequest().authenticated()
+//			.anyRequest().hasAuthority("ROLE_ADMIN")
+			.and()
+				.addFilter(new JWTAuthenticationFilter(authenticationManager()))
+				.addFilter(new JWTAuthorizationFilter(authenticationManager()));
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// Se define la clase que recupera los usuarios y el algoritmo para procesar las passwords
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS", "PUT", "DELETE"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("*", "http://localhost:4200/signin"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues().combine(corsConfiguration));
+		return source;
+	}
+>>>>>>> e5adaa7 CORS
 }
